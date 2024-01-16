56e0a26e5388278be892e2f0e5c3808098f24aae
alissacheng <alissabc@hotmail.com>
2024-01-16 00:46:17 -0500

    add load function

--- assets/air_quality.py
+++ assets/air_quality.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from connectors.air_quality_api import AirQualityApiClient
+from connectors.posgresql import PostgreSqlClient
 from pathlib import Path
+from sqlalchemy import Table, MetaData, Column, Integer, String, Float
 
 
 def extract_air_quality(
@@ -54,3 +56,37 @@ def transform(df_aq: pd.DataFrame, df_cities: pd.DataFrame) -> pd.DataFrame:
     df_selected['air_pollution_level'] = pd.cut(df_selected['aqi'], [0, 50, 100, 150, 200, 300, 1000], labels = ['Good', 'Moderate', 'Unhealthy if Sensitive', 'Unhealthy', 'Very Unhealthy', 'Hazardous'])
 
     return df_selected
+
+def load(
+    df: pd.DataFrame,
+    postgresql_client: PostgreSqlClient,
+    table: Table,
+    metadata: MetaData,
+    load_method: str = "overwrite",
+) -> None:
+    """
+    Load dataframe to either a database.
+
+    Args:
+        df: dataframe to load
+        postgresql_client: postgresql client
+        table: sqlalchemy table
+        metadata: sqlalchemy metadata
+        load_method: supports one of: [insert, upsert, overwrite]
+    """
+    if load_method == "insert":
+        postgresql_client.insert(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    elif load_method == "upsert":
+        postgresql_client.upsert(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    elif load_method == "overwrite":
+        postgresql_client.overwrite(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    else:
+        raise Exception(
+            "Please specify a correct load method: [insert, upsert, overwrite]"
+        )
--- pipelines/air_quality.py
+++ pipelines/air_quality.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 import os
 from connectors.air_quality_api import AirQualityApiClient
+from connectors.posgresql import PostgreSqlClient
 from sqlalchemy import Table, MetaData, Column, Integer, String, Float
 from assets.air_quality import (
     extract_air_quality,
-    transform
+    transform,
+    load
 )
 import yaml
 from pathlib import Path
@@ -19,7 +21,11 @@ if __name__ == "__main__":
     # LOGGING_PASSWORD = os.environ.get("LOGGING_PASSWORD")
     # LOGGING_PORT = os.environ.get("LOGGING_PORT")
     API_KEY = os.environ.get("API_KEY")
-
+    SERVER_NAME = os.environ.get("SERVER_NAME")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME")
+    DB_USERNAME = os.environ.get("DB_USERNAME")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    PORT = os.environ.get("PORT")
 
     # get config variables
     yaml_file_path = __file__.replace(".py", ".yaml")
@@ -36,11 +42,46 @@ if __name__ == "__main__":
 
     config=pipeline_config.get("config")
 
+    #extract
     df_aq, df_cities = extract_air_quality(
         air_quality_api_client=air_quality_api_client,
         city_reference_path=config.get("city_reference_path"),
     )
 
+    #transform
+    df_transformed = transform(df_aq=df_aq, df_cities=df_cities)
+    print(df_transformed.head())
 
-    df = transform(df_aq=df_aq, df_cities=df_cities)
-    print(df.head())
+    #load
+    postgresql_client = PostgreSqlClient(
+        server_name=SERVER_NAME,
+        database_name=DATABASE_NAME,
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        port=PORT,
+    )
+    metadata = MetaData()
+    table = Table(
+        "air_quality_data",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("iso_datetime", String),
+        Column("city", String),
+        Column("country", String),
+        Column("population", Integer),
+        Column("aqi", Integer),
+        Column("humidity", Float),
+        Column("temperature", Float),
+        Column("pm10", Float),
+        Column("pm2.5", Float),
+        Column("aqi_rank", Integer),
+        Column("population/km2", Integer),
+        Column("air_pollution_level", String),
+    )
+    load(
+        df=df_transformed,
+        postgresql_client=postgresql_client,
+        table=table,
+        metadata=metadata,
+        load_method="upsert",
+    )
